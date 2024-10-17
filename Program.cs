f1c310db686a9fb9dd0856e77afe53dafc9bc4db
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:42:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IndexOutOfRangeExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] series = { 10, 20, 30, 40 };
+            try
+            {
+                for(int i=0;i < series.Length;i++)
+                {
+                    Console.WriteLine(series[5]);
+                }
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
